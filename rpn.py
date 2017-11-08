c1d699c3aa0ba90629eff2fc4bc54cb710bca3b4
Austin Grandison <apgrand@umich.edu>
2017-11-07 21:49:10 -0500

    Final versions

--- rpn.py
+++ rpn.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python3
+
 import operator
 
-ops = {
-	'+': operator.add,
-	'-': operator.sub,
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
 }
 
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
-def calculate(arg):
-	stack = list()
-	for token in arg.split():
-		try:
-			stack.append(int(token))
-		except ValueError:
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			Function = ops(token)
-			result = function(arg1, arg2)
-			stack.append(result)
-	return(stack.pop())
 def main():
     while True:
-        calculate(input("rpn calc> "))
-if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
+
+if __name__ == '__main__':
     main()
